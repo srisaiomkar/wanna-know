57bf137b335429eb9b032aef64c1aa5a7c7a44a9
srisaiomkar <srisaiomkar@gmail.com>
2021-12-23 16:17:36 -0500

    comment create and list

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -5,6 +5,13 @@ export default () => {
   const [title, setTitle] = useState("");
 
   const onSubmit = async (event) => {
+    /* 
+      The preventDefault() method cancels the event if it is cancelable, 
+      meaning that the default action that belongs to the event will not occur. 
+      For example, this can be useful when: 
+      Clicking on a "Submit" button, prevent it from submitting a form. 
+      Clicking on a link, prevent the link from following the URL.
+    */
     event.preventDefault();
 
     await axios.post("http://localhost:4000/posts", {
