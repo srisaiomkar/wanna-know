8066f4585f3db721c05dba1a52c1bf812da46364
srisaiomkar <srisaiomkar@gmail.com>
2021-12-23 14:26:01 -0500

    create post and added bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PostCreate from './PostCreate';
 
-function App(){
-    return(
-        <div>
-            <h1>Hello World!</h1>
+function App() {
+    return (
+        <div className='container'>
+            <h1>Ask a question</h1>
+            <PostCreate />
         </div>
     )
 }
